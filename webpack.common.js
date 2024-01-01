00261bfe84a3545c695ee3d26d7bdbfa4e974640
myinan <m.yasir.inan@gmail.com>
2024-01-02 02:23:41 +0300

    Fix webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -5,6 +5,22 @@ module.exports = {
   entry: {
     app: "./src/index.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,20 +4,4 @@ const common = require("./webpack.common.js");
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
 });
